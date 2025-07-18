cd91463ff789734df49904e5fc13adaab4397c9d
bdiep <bdiep@uw.edu>
2025-07-18 15:52:47 -0700

    add insertions for customer

---
+++ Queries/CUSTOMER.sql
@@ -0,0 +1,50 @@
+INSERT INTO CUSTOMER 
+(first_name, last_name, phone, email, date_of_birth, allergies, preferred_color, loyalty_points, registration_date, is_active) 
+VALUES 
+('Todd', 'Sims', '618-845-8073', 'marisaortega@castro.com', '1996-10-11', 'None', 
+'Purple', 174, '2024-12-31', 1);
+INSERT INTO CUSTOMER 
+(first_name, last_name, phone, email, date_of_birth, allergies, preferred_color, loyalty_points, registration_date, is_active) 
+VALUES 
+('Robin', 'Harris', '905.579.7081x01', 'bentleykelly@yahoo.com', '2000-01-19', 'Pollen', 
+'Red', 86, '2023-01-19', 1);
+INSERT INTO CUSTOMER 
+(first_name, last_name, phone, email, date_of_birth, allergies, preferred_color, loyalty_points, registration_date, is_active) 
+VALUES 
+('Nancy', 'Deleon', '001-227-854-978', 'kmccoy@hotmail.com', '1959-10-27', 'Pollen', 
+NULL, 289, '2023-11-09', 1);
+INSERT INTO CUSTOMER 
+(first_name, last_name, phone, email, date_of_birth, allergies, preferred_color, loyalty_points, registration_date, is_active) 
+VALUES 
+('Raymond', 'Miller', '+1-468-626-2871', 'bradleyalvarez@torres.org', '1960-01-15', 'Fragrance', 
+'Red', 120, '2023-10-07', 1);
+INSERT INTO CUSTOMER 
+(first_name, last_name, phone, email, date_of_birth, allergies, preferred_color, loyalty_points, registration_date, is_active) 
+VALUES 
+('Alyssa', 'Jackson', '(403)441-6240x1', 'vanessa85@gmail.com', '1998-10-31', 'Dust', 
+'Pink', 284, '2025-06-10', 1);
+INSERT INTO CUSTOMER 
+(first_name, last_name, phone, email, date_of_birth, allergies, preferred_color, loyalty_points, registration_date, is_active) 
+VALUES 
+('Hailey', 'Webb', '+1-906-416-5220', 'anthonysmith@hotmail.com', '1965-06-06', 'Pollen', 
+'Red', 10, '2024-01-28', 0);
+INSERT INTO CUSTOMER 
+(first_name, last_name, phone, email, date_of_birth, allergies, preferred_color, loyalty_points, registration_date, is_active) 
+VALUES 
+('Jessica', 'Irwin', '731.049.6021x13', 'jason30@cowan.com', '1967-12-17', NULL, 
+'Red', 16, '2022-09-07', 1);
+INSERT INTO CUSTOMER 
+(first_name, last_name, phone, email, date_of_birth, allergies, preferred_color, loyalty_points, registration_date, is_active) 
+VALUES 
+('Tina', 'Rhodes', '037.456.5659', 'cchase@pennington.com', '1961-10-10', 'Pollen', 
+'Green', 144, '2024-01-07', 1);
+INSERT INTO CUSTOMER 
+(first_name, last_name, phone, email, date_of_birth, allergies, preferred_color, loyalty_points, registration_date, is_active) 
+VALUES 
+('Anne', 'Johnson', '001-866-823-719', 'kelly94@hotmail.com', '1981-04-27', 'Nail polish remover', 
+'Pink', 149, '2024-12-09', 1);
+INSERT INTO CUSTOMER 
+(first_name, last_name, phone, email, date_of_birth, allergies, preferred_color, loyalty_points, registration_date, is_active) 
+VALUES 
+('Rachel', 'Wheeler', '407-484-5977x75', 'davismorgan@watson.com', '1995-03-19', 'None', 
+'Red', 124, '2023-03-12', 0);
